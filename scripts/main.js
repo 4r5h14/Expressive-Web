d437f5fd2f75c74f22dcf9e1fcfd379b00383f2c
Arshia Lodhi <arshia.lodhi@gmail.com>
2023-12-12 19:18:13 -0500

    updated code and added image

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 
 
 let btn = document.querySelector('.Laundry');
-
 btn.addEventListener('click', function(){
     let showMe = document.querySelector('.Laundry');
        this.remove(showMe);   
@@ -9,7 +8,6 @@ btn.addEventListener('click', function(){
 
 
 let disappear = document.querySelector('.Clothes');
-
 disappear.addEventListener('click', function(){
   let clothes = document.querySelector(".Clothes");
   this.remove(clothes);
@@ -18,33 +16,98 @@ disappear.addEventListener('click', function(){
 
 
 let disappear2 = document.querySelector('.Clothes2');
-
 disappear2.addEventListener('click', function(){
   let clothes = document.querySelector(".Clothes2");
-  this.remove(clothes);
+  // Clone the original element
+  var duplicate = disappear2.cloneNode(true);
+
+  // Modify the position of the duplicate 
+  var offsetX = 100;
+  var offsetY = 300;
+
+  // Calculate new position based on the original position
+  var originalRect = disappear2.getBoundingClientRect();
+  var newLeft = originalRect.left + offsetX;
+  var newTop = originalRect.top + offsetY;
+
+  // Set the position of the duplicate
+  duplicate.style.left = newLeft + 'px';
+  duplicate.style.top = newTop + 'px';
+
+  // Append the duplicate to the document
+  document.body.appendChild(duplicate);
+
+
+  
+  //remove both objects
+  disappear2.remove();
+  duplicate.addEventListener('click', function () {
+    duplicate.remove();
+  });
+
 });
 
 
 
 let lightleft = document.querySelector('.light1');
-
 lightleft.addEventListener('click', function() {
-    document.body.style.backgroundColor = "red";
-   
-    
+    document.body.style.backgroundColor = "red";    
 });
 
-let lightright = document.querySelector('.light2');
 
+let lightright = document.querySelector('.light2');
 lightright.addEventListener('click', function() {
        document.body.style.backgroundColor = "yellow";
     
 });
 
+
 let button = document.querySelector('.Switch');
 button.addEventListener('click', function(){
-  document.body.style.background = "white";
-
-  
+  document.body.style.background = "white";  
 });
 
+
+
+let cactus = document.querySelector('.Cactus');
+cactus.addEventListener('click', function(){
+  cactus.style.transition = 'transform 0.3s ease-in-out;'
+  cactus.style.transform = 'rotate(0deg)';
+  cactus.style.position = 'absolute';
+  cactus.style.top = '36%';
+  cactus.style.right = '22%'; 
+});
+
+
+let sidetable = document.querySelector('.SideTableLeft');
+sidetable.addEventListener('click', function () {
+  sidetable.style.transform = 'rotate(0deg)';
+  sidetable.style.position = 'absolute';
+  sidetable.style.top = '35%';
+  sidetable.style.left = '21.5%';
+});
+
+
+let rightsidetable = document.querySelector('.SideTableRight');
+rightsidetable.addEventListener('click', function () {
+  rightsidetable.style.transform = 'rotate(0deg)';
+  rightsidetable.style.position = 'absolute';
+  rightsidetable.style.top = '38%';
+  rightsidetable.style.right = '21%';
+});
+
+let phone = document.querySelector('.Phone');
+phone.addEventListener('click', function () {
+  phone.style.transform = 'rotate(0deg)';
+  phone.style.position = 'absolute';
+  phone.style.top = '34%';
+  phone.style.right = '26%';
+})
+
+let painting = document.querySelector('.p5painting');
+painting.addEventListener('click', function () {
+  painting.style.transform = 'rotate(0deg)';
+  painting.style.position = 'absolute';
+  painting.style.top = '10%';
+  painting.style.right = '28%';
+})
